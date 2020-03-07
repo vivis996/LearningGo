26dd3baab287546e06a7241aec29abb39241e316
Daniel Viveros <dviveros90@gmail.com>
2020-03-07 15:34:21 -0500

    Cards were created by new function "newDeck"

--- cards/deck.go
+++ cards/deck.go
@@ -7,6 +7,19 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
