a4b4de38452dd15cbe0d5b5b604a093e4d390662
Daniel Viveros <dviveros90@gmail.com>
2020-03-08 20:42:37 -0500

    Structs with similar functions were created

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// Very custom logic for generating an english greeting
+	return "Hello There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
 }
