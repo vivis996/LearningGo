29940c202e875c618809ddad3c4977e12d8106cf
Daniel Viveros <dviveros90@gmail.com>
2020-04-04 18:50:38 -0500

    Initial file for channel's lesson

---
+++ channels/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, link := range links {
+
+	}
+}
